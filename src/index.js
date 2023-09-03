5deb56324f42e33cbbd8a709698d184d5bec96b8
Jamaal Ahmed <jamaal729@yahoo.com>
2023-09-02 22:43:16 -0600

    Add db config for GitHub pages

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import Application from "components/Application";
 
 import axios from "axios";
 
+// Netlify
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
+
+// GitHub Pages
+axios.defaults.baseURL = "https://scheduler-api-j729.up.railway.app/"
 
 ReactDOM.render(<Application />, document.getElementById("root"));
